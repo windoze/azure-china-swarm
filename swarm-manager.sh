35ada04fb4b26d25195fdfb56614b668f3c28bca
Chen Xu <windoze.net@gmail.com>
2016-09-20 01:24:28 +0800

    Use proxy for docker engine installation and yum

--- swarm-manager.sh
+++ swarm-manager.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+PROXY=cxp.eastasia.cloudapp.azure.com:80
+
+# Set yum proxy
+echo "proxy=http://${PROXY}" >> /etc/yum.conf
+
 # Install docker engine
 yum -y update
-curl -sSL https://get.docker.com/ | sh
+curl -x ${PROXY} -sSL https://get.docker.com/ | sh
 yum install -y docker-selinux
 
 # Uses my proxy
 mkdir -p /etc/systemd/system/docker.service.d
 cat << EOF > /etc/systemd/system/docker.service.d/http-proxy.conf
 [Service]
-Environment="HTTP_PROXY=http://cxp.eastasia.cloudapp.azure.com:80/"
+Environment="HTTP_PROXY=http://${PROXY}/"
 EOF
 systemctl daemon-reload
 systemctl restart docker
