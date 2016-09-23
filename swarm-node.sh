84a57f00ec8a9ca9f3f3d2317ed28f30351b4f7f
Chen Xu <windoze.net@gmail.com>
2016-09-23 17:01:34 +0800

    optimiza

--- swarm-node.sh
+++ swarm-node.sh
@@ -26,9 +26,10 @@ ExecStart=
 ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock
 EOF
 
-sudo usermod -aG docker "${USERNAME}"
+usermod -aG docker "${USERNAME}"
 systemctl daemon-reload
 systemctl restart docker
+chkconfig docker on
 
 # Get worker token
 TOKEN="`curl -s 10.0.0.4:8080/worker`"
