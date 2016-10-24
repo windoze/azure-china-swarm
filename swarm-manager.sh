791854e781477f0ef8d9596b62a2d314b144e5d1
Chen Xu <windoze.net@gmail.com>
2016-10-25 02:06:16 +0800

    Install docker-compose

--- swarm-manager.sh
+++ swarm-manager.sh
@@ -25,6 +25,10 @@ usermod -aG docker "${USERNAME}"
 systemctl daemon-reload
 systemctl restart docker
 
+# Install docker-compose
+curl -x "${PROXY}" -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
 # Get manager token
 TOKEN="`curl -s 10.0.0.4:8080/manager`"
 
